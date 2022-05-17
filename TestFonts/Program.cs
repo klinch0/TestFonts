d985038be723f47766fadd10fbf1f7e4acfae001
К Клин <kklinchenkov@inbox.ru>
2022-05-17 15:02:50 +0300

    Добавьте файлы проекта.

---
+++ TestFonts/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+
+namespace TestFonts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+                var fontFamilies = installedFontCollection.Families;
+                var fonts = new List<string>();
+                foreach (var fontName in fontFamilies)
+                {
+                    Console.WriteLine($"{fontName.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            while (true) { }
+        }
+    }
+}
